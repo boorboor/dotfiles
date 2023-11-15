62386b40fedea0585534d859c128aba5d48656fd
Massoud Boorboor <boorboor@gmail.com>
2023-11-16 00:57:54 +0100

    Migrate to ruff-lsp

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -160,8 +160,8 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
--- sudo pip install 'python-lsp-server[all]'
-require('lspconfig').pylsp.setup {
+-- pip install ruff-lsp
+require('lspconfig').ruff_lsp.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
