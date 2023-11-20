7e747fadf295b1f88a29eb91af2ed42675a19a1b
Massoud Boorboor <boorboor@gmail.com>
2023-11-20 19:54:59 +0100

    Make ruff-lsp auxiliary

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -160,6 +160,12 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
+--- sudo pip install 'python-lsp-server[all]'
+require('lspconfig').pylsp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
 -- pip install ruff-lsp
 require('lspconfig').ruff_lsp.setup {
   on_attach = on_attach,
