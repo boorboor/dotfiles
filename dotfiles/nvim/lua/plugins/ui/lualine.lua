05e9e76e9f0c1b638e3f5388024f07c5630be51b
Massoud Boorboor <boorboor@gmail.com>
2026-04-30 19:11:05 +0200

    Better lua line

--- dotfiles/nvim/lua/plugins/ui/lualine.lua
+++ dotfiles/nvim/lua/plugins/ui/lualine.lua
@@ -9,6 +9,17 @@ return {
         section_separators = "",
       },
       sections = {
+        lualine_b = {
+          {
+            function()
+              return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+            end,
+            icon = "󰉖",
+          },
+          "branch",
+          "diff",
+          "diagnostics",
+        },
         lualine_c = {
           {
             "filename",
